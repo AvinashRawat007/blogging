a05981c8a96d749504ca9ea48860165d90649155
AvinashRawat007 <71925845+AvinashRawat007@users.noreply.github.com>
2022-03-22 23:58:35 +0530

    Update firebase.js

--- anirban-blogging/public/js/firebase.js
+++ anirban-blogging/public/js/firebase.js
@@ -1,11 +1,6 @@
 let firebaseConfig = {
     // Enter your firebase credentials
-    apiKey: "AIzaSyATFHRjUf4QANdbCqsrZI5HprL-BSzwYrE",
-    authDomain: "blogging-website-867e9.firebaseapp.com",
-    projectId: "blogging-website-867e9",
-    storageBucket: "blogging-website-867e9.appspot.com",
-    messagingSenderId: "231956327249",
-    appId: "1:231956327249:web:82145fe14c221a46374170"
+    
 };
 
 firebase.initializeApp(firebaseConfig);
